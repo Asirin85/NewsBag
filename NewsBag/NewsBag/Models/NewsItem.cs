7fb88ef977d45b4d9513711eb72b2057cbc35280
Dmitriy Gritsenko <dmitriy_gritsenko@epam.com>
2022-05-06 23:58:57 +0400

    model tests added

--- NewsBag/NewsBag/Models/NewsItem.cs
+++ NewsBag/NewsBag/Models/NewsItem.cs
@@ -5,21 +5,120 @@ namespace NewsBag.Models
 {
     public class NewsItem
     {
+        
+        private string id;
         [PrimaryKey]
-        public string ID { get; set; }
-        public string Source { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    id = Guid.NewGuid().ToString();
+                }
+                else
+                    id = value;
+            }
+        }
+        private string source;
+        public string Source
+        {
+            get
+            {
+                return source;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    source = "";
+                }
+                else
+                    source = value;
+            }
+        }
+        private string title;
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    title = "No title";
+                }
+                else
+                    title = value;
+            }
+        }
+        private string description;
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    description = "";
+                }
+                else
+                    description = value;
+            }
+        }
         public DateTimeOffset Date { get; set; }
         public string Link { get; set; }
+        private string imageLink;
         public string ImageLink
         {
-            get; set;
+            get
+            {
+                return imageLink;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    imageLink = "";
+                }
+                else
+                    imageLink = value;
+            }
         }
-        public int ImageExist { get; set; } = 2;
+        private int imageExist;
+        public int ImageExist
+        {
+            get
+            {
+                return imageExist;
+            }
+            set
+            {
+                if (value != 1 || value != 2)
+                {
+                    imageExist = ImageLink.Length > 0 ? 1 : 2;
+                }
+                else
+                    imageExist = value;
+            }
+        }
         public NewsItem()
         {
-
+            ID = Guid.NewGuid().ToString();
+            Description = "";
+            Title = "No title";
+            ImageLink = "";
+            ImageExist = 2;
+            Source = "";
         }
         public NewsItem(string iD, string source, string title, string description, DateTimeOffset date, string link, string imageLink, int imageExist)
         {
---
+++ NewsBagTests/NewsItemsTests.cs
@@ -0,0 +1,27 @@
+﻿using NewsBag.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsBagTests
+{
+    public class NewsItemsTests
+    {
+        [Test]
+        public void TestForNewsItemsModel()
+        {
+            var date = DateTimeOffset.Parse("Fri, 06 May 2022 20:55:44 +0300", CultureInfo.InvariantCulture).LocalDateTime;
+            var model = new NewsItem(null,null,null,null, date, null,null,0);
+            Assert.That(!string.IsNullOrEmpty(model.ID));
+            Assert.AreEqual("", model.Source);
+            Assert.AreEqual("No title", model.Title);
+            Assert.AreEqual("", model.Description);
+            Assert.AreEqual("", model.ImageLink);
+            Assert.AreEqual(2, model.ImageExist);
+        }
+    }
+}
